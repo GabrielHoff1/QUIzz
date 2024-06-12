6482a94feea500c38e794cfc6a3807746ab1a8ba
GabrielHoff1 <167206051+GabrielHoff1@users.noreply.github.com>
2024-06-12 09:24:28 -0300

    push

---
+++ Estudosjogos-Gabriel Leal/Assets/Navinha/Scripts/BackGround.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackGround : MonoBehaviour
+{
+    [Header("Velocidade")]
+    public float velocidade;
+
+    [Header("Ultimo BackGround")]
+    public GameObject bgObjeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, velocidade * Time.deltaTime, 0);
+        if(transform.position.y <= -12)
+        {
+            transform.position = new Vector3(0, bgObjeto.transform.position.y + 12, 0);
+        }
+    }
+}
